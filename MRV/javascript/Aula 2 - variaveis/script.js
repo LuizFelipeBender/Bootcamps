1fa21c1d0f04b4654fb8d0e9e97d4f5b25d2c1ba
Luiz Bender <LuizBender@outlook.com.br>
2022-01-11 23:05:26 -0300

    main

---
+++ MRV/javascript/Aula 2 - variaveis/script.js
@@ -0,0 +1,26 @@
+//tipos primitivos
+
+//bolean
+var vOUf = false;
+console.log(typeof(vOUf))
+
+//number 
+var number = 150;
+console.log(typeof(number))
+
+//string 
+var string = "Luiz";
+
+//function
+var funcao = function() {}
+console.log(typeof(funcao));
+
+//como declarar
+variable = 'bender';
+console.log(variable);
+
+let variable = 'Felipe';
+variable = 'luiz';
+console.log(variable);
+
+const a = 'Luiz Felipe Alves Bender';
