65672bef7b365487a380c40ecee04f723df84410
Luiz Felipe Alves Bender <85036417+LuizFelipeBender@users.noreply.github.com>
2022-01-30 15:52:36 -0300

    Add files via upload

---
+++ GFT/AbstracaoGame/Program.cs
@@ -0,0 +1,9 @@
+﻿Knight arus = new Knight("Arus", 23, "Knight");
+BlackWizard topapa = new BlackWizard(@"White beard", 42, "Black Wizard");
+Ninja wedge = new Ninja("Wedge", 112, "Ninja");
+Wizard jennica = new Wizard("Jennica",17, "White Wizard");
+
+Console.WriteLine(arus.Attack());
+Console.WriteLine(wedge.Attack());
+Console.WriteLine(jennica.Attack());
+Console.WriteLine(topapa.Attack());
